2db2bca48eba364082ca3a15417851795f32b045
Josh Dean <josh.dean@imtglobalinc.com>
2026-04-13 13:40:30 -0700

    formula now installs to etc/imt-agent-client

--- Formula/imt-agent-client.rb
+++ Formula/imt-agent-client.rb
@@ -3,8 +3,9 @@ class ImtAgentClient < Formula
   homepage ""
   url "https://mop-agent-releases.s3.us-west-2.amazonaws.com/homebrew/prerelease/imt-agent-client-2026.4.9.1.tar.gz"
   sha256 "c5f6a16fae6db0794c1fe123703091482eea8ef5baeda9eac2f6caea3fe86e89"
+  version "2026.4.9.1"
 
   def install
-    etc.install Dir["*"]
+    pkgetc.install Dir["*"]
   end
 end  
