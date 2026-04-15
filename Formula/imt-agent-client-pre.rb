2d96b7a98fe50fc1938370c9ec8d51a081e44513
Josh Dean <josh.dean@imtglobalinc.com>
2026-04-15 16:45:22 -0700

    Formula: removed service root requirement. Agent Client now at 2026.4.16.1

--- Formula/imt-agent-client-pre.rb
+++ Formula/imt-agent-client-pre.rb
@@ -1,8 +1,8 @@
 class ImtAgentClientPre < Formula
   desc "Prerelease version of the remote management software for IMT Engineers"
   homepage ""
-  url "https://mop-agent-releases.s3.us-west-2.amazonaws.com/homebrew/prerelease/imt-agent-client_2026.4.15.5.tar.gz"
-  sha256 "41b9e6623e3a2c16f1b0746f6a7dbe266dbda5240475cb0159945120db939aca"
+  url "https://mop-agent-releases.s3.us-west-2.amazonaws.com/homebrew/prerelease/imt-agent-client_2026.4.16.1.tar.gz"
+  sha256 "ad7bf05e789496ee24c13440c35ebec3f7809ea4a6c9eb2157ec8d091685254f"
 
   def install
     prefix.install Dir["*"]
@@ -18,7 +18,6 @@ class ImtAgentClientPre < Formula
     run opt_prefix/"AgentClient"
     run_type :immediate
     keep_alive true
-    require_root true
     environment_variables PATH: std_service_path_env
   end
 end 
