9acdc0cb249096148b52c2aa4aa6a7b196db5ce8
Josh Dean <josh.dean@imtglobalinc.com>
2026-04-16 10:41:11 -0700

    Agent Client now at 2026.4.16.5

--- Formula/imt-agent-client-pre.rb
+++ Formula/imt-agent-client-pre.rb
@@ -1,8 +1,8 @@
 class ImtAgentClientPre < Formula
   desc "Prerelease version of the remote management software for IMT Engineers"
   homepage ""
-  url "https://mop-agent-releases.s3.us-west-2.amazonaws.com/homebrew/prerelease/imt-agent-client_2026.4.16.4.tar.gz"
-  sha256 "8620052a090b5bdb6fadbd92293d8b3835adc599c99cbf66712f840db003cd66"
+  url "https://mop-agent-releases.s3.us-west-2.amazonaws.com/homebrew/prerelease/imt-agent-client_2026.4.16.5.tar.gz"
+  sha256 "d016d527301a8d68b2a822acfe146be386b222a2e6a544dfb4277cc7ff5d6336"
 
   def install
     prefix.install Dir["*"]
